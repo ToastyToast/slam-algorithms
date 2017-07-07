c5c5ce57aaef9918c325ae7a913ddad70f159ebb
Artem Kovalev <leon112rus@gmail.com>
2017-07-07 11:07:00 +0200

    low variance resampling

--- slam/particle_filter.py
+++ slam/particle_filter.py
@@ -1,10 +1,8 @@
 import numpy as np
 from slam.utils import normalize_angle
 
-def low_variance_resampling(particles):
-    pass
 
-class ParticleFilterLocalization:
+class ParticleFilter:
     def __init__(self, init_pose, num_particles, motion_model, sampler,
     resampler):
         if not motion_model or not sampler:
@@ -12,11 +10,12 @@ class ParticleFilterLocalization:
         if not resampler:
             raise RuntimeErro('Provide resampling algorithm')
         if num_particles < 1:
-            raise RuntimeError('Number of particles must be > 1')
+            raise RuntimeError('Number of particles must be positive')
 
         self.particles = []
         self.motion_model = motion_model
         self.sampler = sampler
+        self.resampler = resampler
 
         self._init(init_pose, num_particles)
 
@@ -36,6 +35,25 @@ class ParticleFilterLocalization:
         pass
 
     def resample(self):
-        pass
+        self.particles = self.resampler(self.particles)
+
+
+def low_variance_resampling(particles):
+    new_particles = []
+
+    Jinv = 1 / len(particles)
+    r = np.random.normal(0, Jinv)
+    # weight
+    c = particles[0][0]
+    i = 1
+    for j in range(0, len(particles)):
+        U = r + (j - 1) * Jinv
+        while U > c:
+            i = i + 1
+            c = c + particles[i][0]
+        new_particles.append(particles[i])
+
+    return new_particles
+
 
 
