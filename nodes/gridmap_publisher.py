Broadcast robot_base transform TODO: Fix it

nodes/gridmap_publisher.py
@@ -5,6 +5,8 @@ import sys
 from slam.gridmap import GridMap, LaserDataMatlab
 
 import rospy
+import tf, tf2_ros
+import geometry_msgs.msg
 from std_msgs.msg import Header
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 from rospy.numpy_msg import numpy_msg
@@ -34,7 +36,7 @@ def gridmap_publisher():
     # map_meta.map_load_time = rospy.Time().now()
     map_meta.resolution = gridmap._grid_size
 
-    grid_map = gridmap.get_prob_map().T * 100
+    grid_map = gridmap.get_prob_map() * 100
     grid_map = grid_map.astype(np.int8)
 
     # Width = cols
@@ -42,9 +44,18 @@ def gridmap_publisher():
     # Height = rows
     map_meta.height = grid_map.shape[0]
 
-    map_meta.origin.position.x = gridmap._offset[1]
-    map_meta.origin.position.y = gridmap._offset[0]
+    map_meta.origin.position.x = gridmap._offset[0]
+    map_meta.origin.position.y = gridmap._offset[1]
     map_meta.origin.position.z = 0
+
+    # Broadcast robot transform
+
+    pose_tf_br = tf2_ros.TransformBroadcaster()
+
+    rbase_tf = geometry_msgs.msg.TransformStamped()
+    rbase_tf.header.stamp = rospy.Time.now()
+    rbase_tf.header.frame_id = 'map'
+    rbase_tf.child_frame_id = 'robot_base'
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
@@ -57,8 +68,26 @@ def gridmap_publisher():
         except StopIteration:
             robot_pose = laser_data.get_pose(timestep_len-1)
 
-        grid_map = gridmap.get_prob_map().T * 100
+        grid_map = gridmap.get_prob_map() * 100
         grid_map = grid_map.astype(np.int8)
+
+        rx = robot_pose.item(0)
+        ry = robot_pose.item(1)
+        rtheta = robot_pose.item(2)
+
+        rospy.loginfo('%s %s %s', rx, ry, rtheta)
+
+        rbase_tf.transform.translation.x = rx
+        rbase_tf.transform.translation.y = ry
+        rbase_tf.transform.translation.z = 0
+
+        quat = tf.transformations.quaternion_from_euler(0, 0, rtheta)
+        rbase_tf.transform.rotation.x = quat[0]
+        rbase_tf.transform.rotation.y = quat[1]
+        rbase_tf.transform.rotation.z = quat[2]
+        rbase_tf.transform.rotation.w = quat[3]
+
+        pose_tf_br.sendTransform(rbase_tf)
 
         h = Header()
         h.stamp = rospy.Time.now()

src/slam/gridmap.py
@@ -60,7 +60,7 @@ class LaserDataMatlab:
         gridmap._map_size = tuple([math.ceil(dim/gridmap._grid_size) for dim in gridmap._map_size_meters])
 
         log_odds_prior = prob2log(gridmap._prior)
-        gridmap._grid_map = np.ones(gridmap._map_size).T * log_odds_prior
+        gridmap._grid_map = np.ones(gridmap._map_size) * log_odds_prior
 
 
 class GridMap:
@@ -105,10 +105,10 @@ class GridMap:
             bres_points = bresenham_line((rx, ry), (lx, ly))
             for point in bres_points:
                 px, py = point
-                map_update[py, px] = self._grid_map[py, px] + \
+                map_update[px, py] = self._grid_map[px, py] + \
                     prob2log(self._prob_free)
 
-            map_update[py, px] = self._grid_map[py, px] + \
+            map_update[px, py] = self._grid_map[px, py] + \
                 prob2log(self._prob_occ)
 
         return map_update, robot_pose_map_frame, laser_end_map_frame